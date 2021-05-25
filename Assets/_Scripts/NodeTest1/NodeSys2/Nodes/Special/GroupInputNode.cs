35988c121766e06ce6817abb8cf5f99509a20807
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2021-05-24 21:32:35 -0500

    start work on groups

---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Nodes/Special/GroupInputNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nodeSys2;
+
+public class GroupInputNode : Node
+{
+    public Property input, name, output;
+
+    public GroupInputNode(bool x)
+    {
+        base.nodeDisc = "Group Input";
+        input = CreateInputProperty("DataIn", false, new Evaluable());
+        input.visible = false;
+        name = CreateInputProperty("Data tag", false, new StringData("input"));
+        output = CreateOutputProperty("output");
+    }
+
+    public override void Handle()
+    {
+        output.Invoke(input.GetData());
+    }
+}
---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Nodes/Special/GroupOutputNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using nodeSys2;
+
+public class GroupOutputNode : Node
+{
+    public Property input;
+    public delegate void GroupOutDelegate(object data);
+    //used to connect to a parent group or instancer node
+    [JsonIgnore] public GroupOutDelegate outDel;
+
+    public GroupOutputNode(bool x)
+    {
+        base.nodeDisc = "Group output";
+        input = CreateInputProperty("input", true, new Evaluable());
+    }
+
+    public override void Handle()
+    {
+        outDel.Invoke(input.GetData());
+    }
+}
