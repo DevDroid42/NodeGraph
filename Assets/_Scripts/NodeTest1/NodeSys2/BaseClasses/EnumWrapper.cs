524191d5dee56c84594eb876dc8d676bb2475228
Ethan Honzik <Honz7449@stthomas.edu>
2023-02-01 09:47:21 -0600

    commit for ENUM testing branch

---
+++ Assets/_Scripts/NodeTest1/NodeSys2/BaseClasses/EnumWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace nodeSys2
+{
+    //JSON.NET cannot correctly deseralize enumerations stored in polymorphic arrays/variables
+    //This class aims to solve that problem by storing a generic version of the ENUM
+    //In Theory the generic type should give JSON.NET the ability to correctly deserialize
+    public class EnumWrapper<T> where T : Enum
+    {
+        public T enumeration;
+    }
+}
