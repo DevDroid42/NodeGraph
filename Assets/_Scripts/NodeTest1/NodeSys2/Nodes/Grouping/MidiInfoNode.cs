1f9562118c4fe09e3aa62586eaf46a30503006d4
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-04-15 15:39:53 -0500

    started to add midi instancer

---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Nodes/Grouping/MidiInfoNode.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nodeSys2;
+
+public class MidiInfoNode : Node
+{
+    [JsonIgnore]
+    public float position, deltaPos, velocity;
+    [JsonProperty] private Property positonProp, deltaPosProp, velocityProp;
+
+    public MidiInstancer(ColorVec pos) : base(pos)
+    {
+        base.nodeDisc = "Midi Info";
+        positonProp = CreateOutputProperty("position");
+        deltaPosProp = CreateOutputProperty("delta position");
+        velocityProp = CreateOutputProperty("velocity");
+    }
+
+    public override void Handle()
+    {
+        positonProp.Invoke(new EvaluableFloat(position));
+        deltaPosProp.Invoke(new EvaluableFloat(deltaPos));
+        velocityProp.Invoke(new EvaluableFloat(velocity));
+    }
+}
---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Nodes/Grouping/MidiInstancer.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nodeSys2;
+
+public class MidiInstancer : StaticInstancer, INetReceivable
+{
+    [JsonProperty] private NetworkReceivableProps netReceiveProps;
+    [JsonProperty] private MidiProperties midiProps;
+    [JsonProperty] private Property output;
+
+    public MidiInstancer(ColorVec pos) : base(pos)
+    {
+        base.nodeDisc = "Midi Receive";
+        netReceiveProps = new NetworkReceivableProps(this);
+        netReceiveProps.dataType.visible = false;
+        netReceiveProps.dataType.SetData(NetworkMessage.DataType.ByteArray);
+        midiProps = new MidiProperties(this);
+        output = CreateOutputProperty("Output");
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        netReceiveProps.init();
+        netReceiveProps.RegisterNetReceive(this);
+        midiProps.notePressedCallback = HandlePressed;
+    }
+
+    private List<int> pressed;
+    private void HandlePressed(List<int> pressed)
+    {
+        this.pressed = pressed;
+    }
+
+    public override void Handle()
+    {
+        if(pressed != null)
+        {
+
+        }
+    }
+
+    public void ReceiveData(NetworkMessage message)
+    {
+        byte[] incoming = message.data;
+        midiProps.UpdateState(incoming);
+    }
+}
