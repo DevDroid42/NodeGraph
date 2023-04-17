98c399543295ca3cfc95e76d8ba77fd760008ff7
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-04-17 11:30:23 -0500

    started to add noise node

---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Dependancies/Graphics/Evaluables/EvaluableNoise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EvaluableNoise : IEvaluable
+{
+    public float scale, offsetx, offsety;
+    public ColorVec EvaluateColor(float vector = 0)
+    {
+        return EvaluateValue(vector);
+    }
+
+    public float EvaluateValue(float vector = 0)
+    {
+        return Mathf.PerlinNoise((vector + offsetx) * scale, offsety * scale);
+    }
+
+    public object GetCopy()
+    {
+        return new EvaluableNoise
+        {
+            scale = scale,
+            offsetx = offsetx,
+            offsety = offsety
+        };
+    }
+
+    public int GetResolution()
+    {
+        return 256;
+    }
+}
---
+++ Assets/_Scripts/NodeTest1/NodeSys2/Nodes/NoiseNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nodeSys2;
+using Newtonsoft.Json;
+
+public class NoiseNode : Node
+{
+    [JsonProperty] Property scale, offsetx, offsety, output;
+    private EvaluableNoise noise;
+
+    public NoiseNode(ColorVec pos) : base(pos)
+    {
+        scale = CreateInputProperty("Scale", true, new EvaluableFloat(1));
+        scale.interactable = true;
+        offsetx = CreateInputProperty("Offset X", true, new EvaluableFloat(1));
+        offsetx.interactable = true;
+        offsety = CreateInputProperty("Offset Y", true, new EvaluableFloat(1));
+        offsety.interactable = true;
+    }
+}
