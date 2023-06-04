b97a60ab9234be6d236716a0a228ce06ed709052
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-06-04 14:32:59 -0500

    started adding evaluable gradient

--- Assets/_Scripts/NodeTest1/NodeSys2/Dependancies/Graphics/Evaluables/EvaluableGradient.cs
+++ Assets/_Scripts/NodeTest1/NodeSys2/Dependancies/Graphics/Evaluables/EvaluableGradient.cs
@@ -2,156 +2,158 @@
 using Newtonsoft.Json.Converters;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-/*
 public class EvaluableGradient : IEvaluable
 {
     [JsonProperty]
-    private SortedDictionary<float, Color> gradientMap;
-    //brightness mutliplier. 
+    private float minPos, maxPos;
+    private ColorVec minColor, maxColor;
+    private int count;
+    private List<ColorVec> colors;
+    private List<float> positions;
 
-    [JsonConverter(typeof(StringEnumConverter))]
-    public enum InterpolationType
-    {
-        linear, closest
-    }
-    public InterpolationType interType = InterpolationType.linear;
+    //brightness mutliplier
+    public EvaluableColorTable.InterpolationType interType = EvaluableColorTable.InterpolationType.linear;
 
-    [JsonConverter(typeof(StringEnumConverter))]
-    public enum ClippingMode
+    public EvaluableGradient()
     {
-        tile, mirror, extend, clip
-    }
-    public ClippingMode clipType = ClippingMode.tile;
-
-    public EvaluableGradient(int keyAmt)
-    {
-        for (int i = 0; i < keyAmt; i++)
-        {
-            ColorVec color = new ColorVec(1);
-            keys.Add(color);
-        }
+        colors = new List<ColorVec>();
+        positions = new List<float>();
     }
 
-    public void SetKey(int index, ColorVec color)
+    public void SetKeyColor(int index, ColorVec color)
     {
-        keys[index] = color;
+        colors[index] = color;
+    }
+
+    public void SetKeyPositon(int index, float position)
+    {
+        positions[index] = position;
+        UpdateMinMax();
+    }
+
+    public void AddKey(float position, ColorVec color)
+    {
+        count++;
+        positions.Add(position);
+        colors.Add(color);
+        UpdateMinMax();
+    }
+
+    public void RemoveKey(int index)
+    {
+        count--;
+        positions.RemoveAt(index);
+        colors.RemoveAt(index);
+        UpdateMinMax();
     }
 
     public int GetkeyAmt()
     {
-        return keys.Count;
+        return colors.Count;
     }
 
+    private void UpdateMinMax()
+    {
+        minPos = positions.Min();
+        minColor = colors[positions.IndexOf(minPos)];
+        maxPos = positions.Max();
+        maxColor = colors[positions.IndexOf(maxPos)];
+    }
+
+    private int GetLeftIndex(float position)
+    {
+        int closestIndex = -1;
+        float bestDelta = float.MaxValue;
+        for (int i = 0; i < count; i++)
+        {
+            float delta = position - positions[i];
+            if (positions[i] < position && (delta < bestDelta))
+            {
+                closestIndex = i;
+                bestDelta = delta;
+            }
+        }
+        return closestIndex;
+    }
+
+    private int GetRightIndex(float position)
+    {
+        int closestIndex = -1;
+        float bestDelta = float.MaxValue;
+        for (int i = 0; i < count; i++)
+        {
+            float delta = positions[i] - position;
+            if (positions[i] > position && (delta < bestDelta))
+            {
+                closestIndex = i;
+                bestDelta = delta;
+            }
+        }
+        return closestIndex;
+    }
 
     private ColorVec Interpolate(float x)
     {
-        int keyCount = keys.Count;
-        switch (keyCount)
+        if (count == 1)
         {
-            case 1:
-                return keys[0];
+            return colors[0];
+        }
+        if(x < minPos)
+        {
+            return minColor;
+        }
+        if(x > maxPos)
+        {
+            return maxColor;
+        }
+
+        // ((light I'm at)/last light)) x (Number of keys)
+        // casted to int = lowest key
+
+        if (x == 1)
+        {
+            x = 0.99999f;
+        }
+        else if (x == 0)
+        {
+            x = 0.00001f;
+        }
+
+        int keyIndex1 = GetLeftIndex(x);
+        ColorVec clr1 = colors[keyIndex1];
+        int keyIndex2 = GetRightIndex(x);
+        ColorVec clr2 = colors[keyIndex2];
+
+        float keyPercent1 = (float)keyIndex1 / (count - 1);
+        float keyPercent2 = (float)keyIndex2 / (count - 1);
+
+        float g = (x - keyPercent1) / (keyPercent2 - keyPercent1);
+        switch (interType)
+        {
+            case EvaluableColorTable.InterpolationType.linear:
+                return ColorOperations.lerp(clr1, clr2, g);
+            case EvaluableColorTable.InterpolationType.closest:
+                if (g < 0.5)
+                {
+                    return clr1;
+                }
+                else
+                {
+                    return clr2;
+                }
+
             default:
-                return normalInterpolate();
+                Debug.Log("Error=====Invalid Interpolation Type======Error");
+                return new ColorVec(0, 0, 255);
         }
-
-        ColorVec normalInterpolate()
-        {
-            // ((light I'm at)/last light)) x (Number of keys)
-            // casted to int = lowest key
-
-            if (x == 1)
-            {
-                x = 0.99999f;
-            }
-            else if (x == 0)
-            {
-                x = 0.00001f;
-            }
-
-            int keyIndex1 = (int)((keyCount - 1) * x);
-            ColorVec clr1 = keys[keyIndex1];
-            int keyIndex2 = (int)((keyCount - 1) * x + 1);
-            ColorVec clr2 = keys[keyIndex2];
-
-            float keyPercent1 = (float)keyIndex1 / (keyCount - 1);
-            float keyPercent2 = (float)keyIndex2 / (keyCount - 1);
-
-            float g = (x - keyPercent1) / (keyPercent2 - keyPercent1);
-            switch (interType)
-            {
-                case InterpolationType.linear:
-                    ColorVec c = new ColorVec((clr2.rx - clr1.rx) * g + clr1.rx, (clr2.gy - clr1.gy) * g + clr1.gy,
-                            (clr2.bz - clr1.bz) * g + clr1.bz);
-                    return c;
-                case InterpolationType.closest:
-                    if (g < 0.5)
-                    {
-                        return clr1;
-                    }
-                    else
-                    {
-                        return clr2;
-                    }
-
-                default:
-                    Debug.Log("Error=====Invalid Interpolation Type======Error");
-                    return new ColorVec(0, 0, 255);
-            }
-        }
-
-
     }
 
     public ColorVec EvaluateColor(float vector)
     {
-        float x = vector;
-        switch (clipType)
-        {
-            case ClippingMode.tile:
-                x = x - (int)x;
-                if (x < 0)
-                {
-                    x = x + 1;
-                }
-                return Interpolate(x);
-            case ClippingMode.mirror:
-                int remain = ((int)x) % 2;
-                x = x - (int)x;
-                if (x < 0)
-                {
-                    x = x + 1;
-                }
-                if (remain != 0)
-                {
-                    x = -x + 1;
-                }
-                return Interpolate(x);
-            case ClippingMode.extend:
-                if (x > 1)
-                {
-                    return Interpolate(1);
-                }
-                if (x < 0)
-                {
-                    return Interpolate(0);
-                }
-                return Interpolate(x);
-            case ClippingMode.clip:
-                if (x > 1)
-                {
-                    return new ColorVec(0, 0, 0, 0);
-                }
-                if (x < 0)
-                {
-                    return new ColorVec(0, 0, 0, 0);
-                }
-                return Interpolate(x);
-            default:
-                Debug.Log("Invalid Clip Type");
-                return new ColorVec(0, 0, 0, 0);
-        }
+        return Interpolate(vector);
     }
 
     public float EvaluateValue(float vector)
@@ -161,18 +163,17 @@ public class EvaluableGradient : IEvaluable
 
     public object GetCopy()
     {
-        EvaluableColorTable temp = new EvaluableColorTable(GetkeyAmt());
-        for (int i = 0; i < temp.GetkeyAmt(); i++)
+        EvaluableGradient gradient = new EvaluableGradient();
+        for (int i = 0; i < count; i++)
         {
-            temp.SetKey(i, keys[i]);
+            gradient.AddKey(positions[i], colors[i]);
         }
-        return temp;
+        return gradient;
     }
 
     public int GetResolution()
     {
-        return keys.Count;
+        return count;
     }
 
 }
-*/
